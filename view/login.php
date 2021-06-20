712407ab9eb0b4c2bc292ee2db9f7224d8c16a88
Michael <6181801041@student.unpar.ac.id>
2021-06-20 12:14:35 +0700

    Update login.php

--- view/login.php
+++ view/login.php
@@ -11,6 +11,13 @@
             <form id="LoginForm" action="enter" method="post" >
                 <input type="text" placeholder="Username" name ="username" required>
                 <input type="password" placeholder="Password" name ="password" required>
+                Role: <select name="role" id="role" required>
+                    <option value="" selected="selected" required>Choose Role</option>
+                    <option value="admin" >Admin</option>
+                    <option value="customer" >Customer</option>
+                    <option value="driver" >Driver</option>
+                </select>
+                <br><br>
                 <button type="submit" class="btn">Login</button>
             </form>
         </div>
