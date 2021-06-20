da4138e7e87425c8f3b5bac5fd11979ddd4c1d8d
MarioBagusPrasetya <6181801071@student.unpar.ac.id>
2021-06-20 13:18:07 +0800

    Update loginController.php

--- controller/loginController.php
+++ controller/loginController.php
@@ -9,27 +9,39 @@ class loginController{
         $this->db = new MySQLDB("localhost","root","","delivery");
     }
 
-    public function view_login(){
-		session_start();
-		if(isset($_SESSION['id']) && isset($_SESSION['username'])){
-			$username = $_SESSION['username'];
-			$id = $_SESSION['id'];
-			header('Location: listUser');
-		}else{
-		return View::createView('login.php',
-			[
-			
-		]);}
+   public function view_login(){
+        session_start();
+        if(isset($_SESSION['id'])){
+            if($_SESSION['role'] == "admin"){
+                    header('Location: listUser');
+                }
+                else if($_SESSION['role'] == "customers"){
+                    header('Location: ');
+                }
+                else if($_SESSION['role'] == "manager"){
+                    header('Location: ');
+                }
+                 else if($_SESSION['driver'] == "drivers"){
+                    header('Location: ');
+                }
+            //$username = $_SESSION['username'];
+            //$id = $_SESSION['id'];
+
+        }else{
+        return View::createView('login.php',
+            []);}
     }
 	
         public function login(){
             $Username = $_POST['username'];
             $Password = $_POST['password'];
-            $result = $this->db->executeSelectQuery("SELECT * FROM admin WHERE username = '$Username' AND password = '$Password'");
-            if(isset($result[0]['username']) && $result[0]['username'] != ''){
+            $Role = $_POST['role'];
+            $result = $this->db->executeSelectQuery("SELECT * FROM $Role WHERE username = '$Username' AND password = '$Password'");
+            if(isset($result[0]['username']) && $result[0]['password'] != ''){
                 session_start();
                 $_SESSION['username'] = $result[0]['username'];
                 $_SESSION['id'] = $result[0]['id'];
+                $_SESSION['role'] = $Role;
             }
         }
 
