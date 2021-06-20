d52d0aee86361831815a237d6e5591398d9e82e4
Michael <6181801041@student.unpar.ac.id>
2021-06-20 14:22:56 +0700

    addDelivery

--- view/addDelivery.php
+++ view/addDelivery.php
@@ -3,11 +3,14 @@
 <a href="">Assign Driver</a>
 <a href="">Help</a>
 <a href="logout">Logout</a>
+<br>
 <?php $idC=$_GET['idCustomer'];?>
-<form method="POST" action="">
+<form id="addDelivery" method="POST" action="">
 <input type="hidden" name="idCustomer" value="<?php echo $idC; ?>" />
 <label>Nama Customer :
-		<input type="text" name="name" value="<?php echo $result[0]->getName();?>" disabled> </label>
+	<input type="text" name="name" value="<?php echo $result[0]->getName();?>" disabled> 
+</label>
+<br>
 <label>Alamat :
 <select name="address">
 <?php foreach ($result as $key => $row) {
@@ -32,7 +35,33 @@
 	} ?>
 </select>
 </label>
-
+<br>
+<br>
+<fieldset>
+<legend>Mode Pengiriman</legend>
+	<input type="radio" name="status" id="now" value="<?php echo date("Y-m-d")?>"/>
+	<label for="status">Kirim Sekarang</label><br>
+	<input type="text" value="<?php echo date("d-m-Y")?>" placeholder="<?php echo date("Y-m-d")?>" disabled>
+	<br>
+	<input type="radio" name="status" id="later" value="Normal Radio" checked= />
+	<label for="status">Kirim Nanti</label><br>
+	<input type="date" value=""/>
+</fieldset>
+<br>
+<table>
+<label>Nama Barang :
+	<input type="text" name="namaBarang" value=""> 
+</label>
+<br>
+<label>Jenis Barang :
+	<input type="text" name="jenisBarang" value=""> 
+</label>
+<br>
+<label>Banyak Barang :
+	<input type="number" name="banyakBarang" value="" min="1" max="10"> 
+</label>
+</table>
+<label>Satuan Barang :
+	<input type="text" name="satuanBarang" value=""> 
+</label>
 </form>
-
-<br> 
