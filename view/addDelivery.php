f6c74176bd3b4858115289661045901a87ac19a5
Michael <6181801041@student.unpar.ac.id>
2021-06-23 22:21:35 +0700

    all

--- view/addDelivery.php
+++ view/addDelivery.php
@@ -7,72 +7,85 @@
 <a href="" class="active" >Kirim</a>
 <a href="">Assign Driver</a>
 <a href="">Help</a>
-
-<?php $idC=$_GET['idCustomer'];?>
-<form id="addDelivery" method="POST" action="addDelivery">
-<input type="hidden" name="idCustomer" value="<?php echo $idC; ?>" />
-<label>Nama Customer :
-	<input type="text" name="name" value="<?php echo $result[0]->getName();?>" disabled> 
-</label>
+</div>
 <br>
-<label>Alamat :
-<select name="address">
-<?php foreach ($result as $key => $row) {
+<?php $idC=$_GET['idCustomer'];?>
+<div id = "container-form">
+	<form  id="addDelivery" method="POST" action="addDelivery">
+		<input type="hidden" name="idCustomer" value="<?php echo $idC; ?>" />
+		<label>Nama Customer :
+			<br>
+			<input type="text" name="name" value="<?php echo $result[0]->getName();?>" disabled> 
+		</label>
+		<br>
+		<label>Alamat :
+			<br><br>
+		<select id = "select" name="address">
+		<?php foreach ($result as $key => $row) {
 			echo "<option value=";
 			echo $row->getAddressId();
 			echo ">";
 			echo $row->getAddress();
 			echo"</option>";
-	} ?>
-</select>
-</label>
-<br>
-<br>
-<label>Driver :
-<select name="drivers">
-<?php foreach ($result2 as $key => $row) {
+		} ?>
+		</select>
+		</label>
+		<br>
+		<br>
+		<label>Driver :
+			<br><br>
+		<select id = "select" name="drivers">
+		<?php foreach ($result2 as $key => $row) {
 			echo "<option value=";
 			echo $row->getIdDriver();
 			echo ">";
 			echo $row->getNameDriver();
 			echo"</option>";
-	} ?>
-</select>
-</label>
-<br>
-<br>
-<fieldset>
-<legend>Mode Pengiriman</legend>
-	<input type="radio" name="status" value="Sekarang" onclick = 'setDisable()' checked/>
-	<label for="status">Kirim Sekarang</label><br>
-	<input type="text" value="<?php echo date("d-m-Y")?>" placeholder="<?php echo date("d-m-Y")?>" disabled>
-	<br>
-	<input type="radio" name="status" value="Nanti" onclick = 'setEnable()'/>
-	<label for="status">Kirim Nanti</label><br>
-	<input type="date" name="tanggalKirim" id="tanggalKirim" disabled/>
-</fieldset>
-<br>
-<table>
-<label>Nama Barang :
-	<input type="text" name="namaBarang" value=""> 
-</label>
-<br>
-<label>Jenis Barang :
-	<input type="text" name="jenisBarang" value=""> 
-</label>
-<br>
-<label>Banyak Barang :
-	<input type="number" name="banyakBarang" value="" min="1" max="10"> 
-</label>
-</table>
-<label>Satuan Barang :
-	<input type="text" name="satuanBarang" value=""> 
-</label>
-<input type="submit" value="SUBMIT">
-<br>
+		} ?>
+		</select>
+		</label>
+		<br>
+		<br>
+		<label for="status">Kirim Sekarang</label>
+		<br>
+		<input id = "radiobtn" type="radio" name="status" value="Sekarang" onclick = 'setDisable()' checked/>
+		<input id = "inputradiobtn" type="text" value="<?php echo date("d-m-Y")?>" placeholder="<?php echo date("d-m-Y")?>" disabled>
+		<br>
+		<label for="status">Kirim Nanti</label>
+		<br>
+		<input id = "radiobtn" type="radio" name="status" value="Nanti" onclick = 'setEnable()'/>
+		<input id = "inputradiobtn" type="date" name="tanggalKirim" id="tanggalKirim" disabled/>
 
-</form>
-<a href="listUser">BACK</a>
+	
+
+		<br>
+		<table>
+		<label>Nama Barang :
+			<br>
+			<input type="text" name="namaBarang" value=""> 
+		</label>
+		<br>
+		<label>Jenis Barang :
+			<br>
+			<input type="text" name="jenisBarang" value=""> 
+		</label>
+		<br>
+		<label>Banyak Barang :
+			<br>
+			<input type="number" name="banyakBarang" value="" min="1" max="10"> 
+		</label>
+		</table>
+		<label>Satuan Barang :
+			<br>
+			<input type="text" name="satuanBarang" value=""> 
+		</label>
+		<br>
+		<button class="btnlist" type="submit" value="SUBMIT">Submit</button>
+		<button class="btnlist" type="submit"><a href="listUser">Back</a></button>
+		<br>
+	</form>
+
+</div>
 
 <script>
 	let tgl = document.getElementById("tanggalKirim");
